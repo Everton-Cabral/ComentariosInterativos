ea769b9fb4f3f34ef52486db1e54e829d0cbca82
Everton <cabralclarinetista@gmail.com>
2022-09-26 20:26:46 -0300

    Corrigendo resto de código

--- src/components/reply/AppReply.vue
+++ src/components/reply/AppReply.vue
@@ -5,7 +5,7 @@
             <img class="logo" :src="require('@/assets'+reply.user.image.png)" alt="Logo">
            <span class="username">{{reply.user.username}}</span> 
            <span class="identifier" v-show="loggedInUser">you</span>
-           <span>{{createdAt}}</span> 
+           <span>{{reply.createdAt}}</span> 
         </div>
         <div class="comentario">
            <span class="replyingTo">{{'@'+reply.replyingTo}} </span>{{reply.content}}
@@ -46,26 +46,7 @@ export default {
             }
             return resultado
         },
-        createdAt(){
-            let resultado
-           if(typeof this.reply.createdAt === 'string'){
-             resultado = this.reply.createdAt
-           } else{
-
-               var createdDay = this.reply.createdAt
-               var  today = new Date('2022/09/30') //Insira para que hoje seja uma data futura para teste
-               const diff = Math.abs(today - createdDay)
-               resultado = Math.ceil(diff / (1000 * 60 * 60 * 24))
-
-               if(resultado <= 7){
-                resultado = `${resultado} days ago`
-               }
-
-              
-           }
-
-            return resultado
-        }
+       
     }
 }
 </script>
